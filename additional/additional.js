25240be9279b74d1bf06a1cf26d616eee8e94ea0
Cherevanmisha <cherevanmisha@ukr.net>
2021-11-16 23:33:53 +0200

    Lesson3

--- additional/additional.js
+++ additional/additional.js
@@ -1,4 +1,4 @@
- // 1. Створити пустий масив та :
+// 1. Створити пустий масив та :
 //     a. заповнити його 50 парними числами за допомоги циклу.
 
 /*let arr = [];
@@ -158,31 +158,90 @@ console.log(arr);*/
 //
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
 //     Записати цей об'єкт в новий масив
-// Example:
-//     let usersWithCities = [
-//             {
-//                 id: 1, // <===
-//                 name: 'vasya',
-//                 age: 31,
-//                 status: false,
-//                 address: {
-//                     user_id: 1, // <===
-//                     country: 'Ukraine',
-//                     city: 'Ternopil'
-//                 }
-//             },
-//             // TO BE CONTINUED .....
-//         ]
 //
+// let usersWithId = [
+//         {id: 1, name: 'vasya', age: 31, status: false},
+//         {id: 2, name: 'petya', age: 30, status: true},
+//         {id: 3, name: 'kolya', age: 29, status: true},
+//         {id: 4, name: 'olya', age: 28, status: false}
+//     ];
 //
-//
-/
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+// let arr2 = [];
+// for (const user of usersWithId) {
+//     for (const city of citiesWithId) {
+//         if (user.id === city.user_id) {
+//             user.id = city;
+//             arr2.push(user)
+//         }
+//     }
+// };
+// console.log(arr2)
+
+
+
+
+
+
+
+
+
 //  - Взяти масив з 10 чисел або створити його.
 //  Вивести в консоль тільки ті елементи, значення яких є парними.
+// let arr = [10, 15, 70, 22, 99, 14, 45, 13, 65, 10];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i])
+//     }
+// }
+
+
+// - Взяти масив з 10 чисел або створити його.
+// Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+// let arr = [10, 15, 70, 22, 99, 14, 45, 13, 65, 10];
+// let arr2 = [];
+// for (let i = 0; i < arr.length; i++) {
+//     arr2.push(arr[i]);
+// }
+// console.log(arr2);
 //
-// - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
-//
+
+
+
+
 //
 // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+
+// let arr = ['a', 'b', 'c'];
+//
+// let arr2=[];
+// for (let i = 0; i < arr.length; i++) {
+//     arr2 =  arr2+arr[i] ;
+// }
+// console.log(arr2)
+
+
 // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+// let arr = ['a', 'b', 'c'];
+// let arr2=[];
+// let i = 0;
+// while (i < arr.length) {
+//     arr2 =  arr2+arr[i] ;
+//     i++;
+// }
+// console.log(arr2)
+
+
+
 // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+// let arr = ['a', 'b', 'c'];
+// let arr2 = [];
+// for (const array of arr) {
+//     arr2 =  arr2 + array ;
+// }
+// console.log(arr2)
